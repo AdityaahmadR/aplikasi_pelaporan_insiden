5139a304ba9a072982f2dec5b9c33bbc57c6fd88
akb2702 <muh.akbar.arjuna270204@gmail.com>
2025-12-04 18:09:33 +0800

    update halaman upload video

--- app/src/main/java/com/example/aplikasipelaporaninsiden/UploadVideo.java
+++ app/src/main/java/com/example/aplikasipelaporaninsiden/UploadVideo.java
@@ -1,13 +1,26 @@
 package com.example.aplikasipelaporaninsiden;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class UploadVideo extends AppCompatActivity {
+
+    private ImageView ivBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upload_video);
+
+        ivBack = findViewById(R.id.ivBack);
+
+        ivBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
