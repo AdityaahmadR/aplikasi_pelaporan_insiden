fd1b53568bdbfee32c490cda29a3e2075d5f8793
akb2702 <muh.akbar.arjuna270204@gmail.com>
2025-12-04 09:22:07 +0800

    Perbaikan Minor

--- app/src/main/java/com/example/aplikasipelaporaninsiden/AdminTampilanLaporanManualLanjutan.java
+++ app/src/main/java/com/example/aplikasipelaporaninsiden/AdminTampilanLaporanManualLanjutan.java
@@ -1,16 +1,41 @@
 package com.example.aplikasipelaporaninsiden;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 public class AdminTampilanLaporanManualLanjutan extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_tampilan_laporan_manual_lanjutan);
 
+        drawerLayout = findViewById(R.id.drawer_layout);
+        ImageView btnSidebar = findViewById(R.id.btnSidebar);
+
+        btnSidebar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
+
         // Since this is a continuation of Laporan Manual, we'll highlight the same button.
         SidebarHelper.setupSidebar(this, R.id.btnLaporanManual);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 }
